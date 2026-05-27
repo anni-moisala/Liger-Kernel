e536efb58b8777035ef788cd0478f1e55f1eb5e0
Anni Moisala <anni.moisala@csc.fi>
2026-05-27 16:32:20 +0300

    Update run_qwen.sh

--- examples/huggingface/run_qwen.sh
+++ examples/huggingface/run_qwen.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
+#SBATCH --argos=no
+#SBATCH --account=project_2001659
+#SBATCH --partition=gpumedium
+#SBATCH --time=15
+#SBATCH --tasks-per-node=1
+#SBATCH --gres=gpu:gh200:4
+#SBATCH --nodes=1
+#SBATCH --mem=240G
+#SBATCH --cpus-per-task=288
 
-torchrun --nnodes=1 --nproc-per-node=4 training.py \
+module purge
+module load csc-tools
+module load python-pytorch
+
+source /scratch/project_2001659/amoisala/liger_kernel_fork/Liger-Kernel/venv/bin/activate
+
+# We are putting the cache in the ramdisk, stored in
+# memory. Alternatively store it to the project's scratch.
+
+export HF_HOME=/scratch/$SLURM_JOB_ACCOUNT/$USER/hf-cache/
+mkdir -p $HF_HOME
+
+python -m torch.distributed.run --nnodes=1 --nproc-per-node=4 training.py \
     --model_name "Qwen/Qwen2-7B" \
-    --bf16 \
+    --bf16 True \
     --num_train_epochs 1 \
     --per_device_train_batch_size 48 \
     --per_device_eval_batch_size 64 \
@@ -19,4 +40,6 @@ torchrun --nnodes=1 --nproc-per-node=4 training.py \
     --fsdp_config config/fsdp_config.json \
     --seed 42 \
     --use_liger True \
-    --output_dir alpaca_finetuning
+    --output_dir alpaca_finetuning \
+    --gradient_checkpointing False \
+    --max_seq_length 512
