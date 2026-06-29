0587f4493f4c3f7c16d871acf9622adee90abec3
anni-moisala <anni.moisala@gmail.com>
2026-06-29 13:11:17 +0300

    restore files

--- examples/huggingface/run_gemma.sh
+++ examples/huggingface/run_gemma.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
-#SBATCH --argos=no
-#SBATCH --account=project_2001659
-#SBATCH --partition=gpumedium
-#SBATCH --time=15
-#SBATCH --tasks-per-node=1
-#SBATCH --gres=gpu:gh200:4
-#SBATCH --nodes=1
-#SBATCH --mem=240G
-#SBATCH --cpus-per-task=288
 
-module purge
-module load csc-tools
-module load python-pytorch
-
-source /scratch/project_2001659/amoisala/liger_kernel_fork/Liger-Kernel/venv/bin/activate
-
-# We are putting the cache in the ramdisk, stored in
-# memory. Alternatively store it to the project's scratch.
-
-export HF_HOME=/scratch/$SLURM_JOB_ACCOUNT/$USER/hf-cache/
-mkdir -p $HF_HOME
-
-python -m torch.distributed.run --nnodes=1 --nproc-per-node=4 training.py \
+torchrun --nnodes=1 --nproc-per-node=4 training.py \
     --model_name "google/gemma-7b-it" \
-    --bf16 True \
+    --bf16 \
     --max_steps 20 \
     --per_device_train_batch_size 24 \
     --per_device_eval_batch_size 1 \
@@ -41,5 +20,3 @@ python -m torch.distributed.run --nnodes=1 --nproc-per-node=4 training.py \
     --seed 42 \
     --use_liger True \
     --output_dir alpaca_finetuning
-    --gradient_checkpointing False \ # check if can be enabled, might affect memory
-    --max_seq_length 512
